0bf07da39bf7b136fd1d60265704f2ae8c3da320
SalageanMircea <77210905+SalageanMircea@users.noreply.github.com>
2025-11-21 00:23:54 +0200

    Add files via upload

---
+++ image_processing.c
@@ -0,0 +1,49 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    int width, height, channels;
+
+    unsigned char *image = stbi_load("doom.png", &width, &height, &channels, 0);
+    FILE *out = fopen("gray_values.txt", "w");
+
+    int x;
+    int y;
+
+    x=0;
+    y=0;
+    while (y != height)
+    {
+         int index = (y * width + x) * channels; //find 2d info in a 1d space (just a formula <3)
+
+        unsigned char r = image[index + 0];
+        unsigned char g = image[index + 1];
+        unsigned char b = image[index + 2];
+
+        unsigned char gray = 0.299 * r + 0.587 * g + 0.114 * b; //based on human visual perception
+
+        unsigned char *gradient  =  "@#W$9876543210?!abc;:+=-,._ ";
+        
+        fprintf(out, "%c ", gradient[(gray * (27 - 1)) / 255]); //formula to map 0->255 gray scale to 0->27 ascii art
+
+
+        if(x==width)
+        { 
+           y++;
+           x=0;
+           fprintf(out, "\n");
+        }
+
+        x++;
+    }
+
+
+    stbi_image_free(image);
+    fclose(out);
+
+    return 0;
+}
